1fe4b54352ce97c61222141f334e9e2a31113fee
Shagina178 <116224389+Shagina178@users.noreply.github.com>
2024-08-08 13:04:02 +0200

    Add files via upload
    Home page latest update.

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,104 +8,113 @@
     <link rel="stylesheet" href="styles.css">
     <script src="https://kit.fontawesome.com/159bedbb65.js" crossorigin="anonymous"></script>
     <style>
-        @media (max-width: 1200px) {
-    .news-card {
-        width: 45%;
-    }
-    .column, .image-box, .video-box {
-        width: 45%;
-    }
-}
+    @media (max-width: 1200px) {
+        .news-card {
+            width: 45%;
+        }
 
-@media (max-width: 768px) {
-    #header nav ul li {
-        display: block;
-        margin: 10px 0;
+        .column,
+        .image-box,
+        .video-box {
+            width: 45%;
+        }
     }
 
-    #header nav i {
-        display: block;
+    @media (max-width: 768px) {
+        #header nav ul li {
+            display: block;
+            margin: 10px 0;
+        }
+
+        #header nav i {
+            display: block;
+        }
+
+        #sidemenu {
+            position: fixed;
+            right: -300px;
+            top: 0;
+            width: 250px;
+            height: 100%;
+            background: #fff;
+            transition: right 0.3s;
+        }
+
+        .news-card {
+            width: 100%;
+        }
+
+        .column,
+        .image-box,
+        .video-box {
+            width: 100%;
+        }
+
+        .ticker-content {
+            display: block;
+        }
+
+        .level-sports-text {
+            font-size: 18px;
+        }
+
+        .logo-letter {
+            font-size: 30px;
+        }
     }
 
-    #sidemenu {
-        position: fixed;
-        right: -300px;
-        top: 0;
-        width: 250px;
-        height: 100%;
-        background: #fff;
-        transition: right 0.3s;
+    @media (max-width: 480px) {
+        .level-sports-text {
+            font-size: 16px;
+        }
+
+        .logo-letter {
+            font-size: 24px;
+        }
     }
 
-    .news-card {
-        width: 100%;
-    }
 
-    .column, .image-box, .video-box {
-        width: 100%;
+    .large-text {
+        font-size: 3em;
+        font-weight: bold;
+        color: #14e1e3;
     }
-
-    .ticker-content {
-        display: block;
-    }
-
-    .level-sports-text {
-        font-size: 18px;
-    }
-
-    .logo-letter {
-        font-size: 30px;
-    }
-}
-
-@media (max-width: 480px) {
-    .level-sports-text {
-        font-size: 16px;
-    }
-
-    .logo-letter {
-        font-size: 24px;
-    }
-}
     </style>
 </head>
+
 <body>
-<div id="header">
-  <nav>
-    <ul id="sidemenu">
-      <li><a href="index.php">Home</a></li>
-      <li><a href="aboutUs.php">About</a></li>
-      <li><a href="#services">Our Shop</a></li>
-      <li><a href="#portfolio">News Updates</a></li>
-      <li><a href="#contact">Services</a></li>
-      <li><a href="#contact">Contact</a></li>
-      <i class="fa-solid fa-xmark" onclick="closemenu()"></i>
-    </ul>
-      <i class="fa-solid fa-bars"
-             style="color: #262626;
+    <div id="header">
+        <nav>
+            <ul id="sidemenu">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#services">Our Shop</a></li>
+                <li><a href="news-update.php">News Updates</a></li>
+                <li><a href="services.php">Services</a></li>
+                <li><a href="#contact">Contact</a></li>
+                <i class="fa-solid fa-xmark" onclick="closemenu()"></i>
+            </ul>
+            <i class="fa-solid fa-bars" style="color: #262626;
              position: fixed;
              top: 5%;
              right: 5%;
              transform: translateY(-50%);
-             font-size: 25px; "
-            onclick="openmenu()"></i>
-  </nav>
-</div>
-  </header>
+             font-size: 25px;" onclick="openmenu()"></i>
+        </nav>
+    </div>
 
-  
-
-<div class="white-overlay">
-    <strong>
-        <div class="logo-container">
-            <span class="logo-letter blue">L</span>
-            <span class="logo-letter red">S</span>
-            <span class="logo-letter green">O</span>
-            <span class="logo-letter yellow">N</span>
-        </div>
+    <div class="logo-area">
+        <strong>
+            <div class="logo-container" id="logoContainer">
+                <span class="logo-letter blue">L</span>
+                <span class="logo-letter red">S</span>
+                <span class="logo-letter yellow">O</span>
+                <span class="logo-letter green">N</span>
+            </div>
         </strong>
-        <div class="level-sports-text">Level Sports Of Namibia</div>
-   
+        <video autoplay loop muted>
+            <source src="videos/SportTransition - Made with Clipchamp.mp4" type="video/mp4">
+        </video>
     </div>
 
     <div class="second-white-overlay">
@@ -157,7 +167,7 @@
     </div>
 
     <div class="full-white-overlay"></div>
-    
+
     <div class="news-section" id="newsSection">
         <div class="news-card">
             <img src="path_to_image1.jpg" alt="News Image" class="news-image">
@@ -195,26 +205,22 @@
 
 
     <div class="background-container">
-        
-       <div class="column" id="column1"></div>
-    <div class="column" id="column2"></div>
-    <div class="column" id="column3"></div>
-    <div class="column" id="column4"></div>
-    <div class="column" id="column5"></div>
-    <div class="column" id="column6"></div>
-    <div class="column" id="column7"></div>
-     <div class="outer"></div>
-        
-        <div class="image-boxes-container">
-        <div class="image-box" style="background-image: url('images/avdert 1.gif');"></div>
-        <div class="video-box">
-            <img src="images/hozie.gif" alt="Hozie Gif">
-        </div>
-        
-    </div>
-    </div>
+        <div class="column" id="column1"></div>
+        <div class="column" id="column2"></div>
+        <div class="column" id="column3"></div>
+        <div class="column" id="column4"></div>
+        <div class="column" id="column5"></div>
+        <div class="column" id="column6"></div>
+        <div class="column" id="column7"></div>
+        <div class="outer"></div>
 
-   
+        <div class="image-boxes-container">
+            <div class="image-box" style="background-image: url('images/avdert 1.gif');"></div>
+            <div class="video-box">
+                <img src="images/hozie.gif" alt="Hozie Gif">
+            </div>
+        </div>
+    </div>
 
     <div class="footer">
         © 2024 by G Mobility Investment
@@ -222,14 +228,26 @@
 
     <script src="script.js"></script>
     <script>
-        var sidemenu= document.getElementById("sidemenu");
+    var sidemenu = document.getElementById("sidemenu");
+    var logoContainer = document.getElementById("logoContainer");
 
-        function openmenu(){
-            sidemenu.style.right="0";
+    function openmenu() {
+        sidemenu.style.right = "0";
+    }
+
+    function closemenu() {
+        sidemenu.style.right = "-300px";
+    }
+
+    window.addEventListener("scroll", function() {
+        if (window.scrollY > 50) {
+            logoContainer.innerHTML = "<span class='large-text'>Level Sports Of Namibia</span>";
+        } else {
+            logoContainer.innerHTML =
+                '<span class="logo-letter blue">L</span><span class="logo-letter red">S</span><span class="logo-letter gren">O</span><span class="logo-letter yellow">N</span>';
         }
-        function closemenu(){
-            sidemenu.style.right="-300px";
-        }
+    });
     </script>
 </body>
+
 </html>
