7519a925c6aa3466b85427034a9b7f4f5bfc09d4
Chilwalo Owen Luyando <chilwaloowen130@gmail.com>
2024-08-19 14:56:42 +0200

    training and development

--- All sports/golf.php
+++ All sports/golf.php
@@ -38,7 +38,7 @@ include 'golf_registration.php';
             <a href="clubs.php">Clubs</a>
             <a href="leagues.php">Leagues</a>
             <a href="../All sports/golfProfile.php">Player Profiles</a>
-            <a href="swimming.php">Training and Development</a>
+            <a href="trainingAndDevelopment.php">Training and Development</a>
             <a href="golfsupporters.php">Supporters Club</a>
             <a href="golf.php">Events</a>
             <a href="History.php">History</a>
